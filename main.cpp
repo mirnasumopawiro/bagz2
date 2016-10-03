8e7e25aed5877f4485d1bb0aaaccf5bf96afe277
Bagus Manuaba <bagus.manuaba@gmail.com>
2016-10-03 08:45:46 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Ida Bagus Kerthyayana Manuaba on 14/09/2016.
+//  Copyright © 2016 Ida Bagus Kerthyayana Manuaba. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    
+    string name;
+    
+    // insert code here...
+    cout << "enter your name here!" << endl;
+    cin >> name;
+    cout << "Hello, World!"+name << endl;
+    
+    return 0;
+}
